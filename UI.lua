4d45b40be48aced59bb37bb5288c55507d4b5ec7
ZaxissNoMalware <nikitazaxiss@gmail.com>
2024-12-11 02:01:28 +0300

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1375,7 +1375,7 @@ local function getObjGen()
             Gui.Logo.Size = UDim2.new(1, 0, 0.100000001, 0)
             Gui.Logo.ZIndex = 122
             Gui.Logo.Image = "http://www.roblox.com/asset/?id=110101115018441"
-            Gui.Logo.ImageColor3 = Color3.fromRGB(134, 142, 255)
+            Gui.Logo.ImageColor3 = Color3.fromRGB(255,255,255)
             Gui.Logo.ScaleType = Enum.ScaleType.Fit
 
             Gui.UIGradient_5.Color =
